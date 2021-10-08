12d9f9cc547f97b71585da42624a89040b45c064
karashevmt <karashevmarem@Gmail.com>
2021-10-08 13:39:43 +0300

    dark

--- js/main.js
+++ js/main.js
@@ -2,6 +2,15 @@ const toggle = document.querySelector('.toggle')
 const topbar = document.querySelector('.topbar')
 const navigation = document.querySelector('.navigation')
 const main = document.querySelector('.main')
+const themeSwitch = document.querySelector('.themeSwitch')
+const body = document.querySelector('body')
+
+
+
+themeSwitch.onclick = function () {
+	body.classList.toggle('dark')
+}
+
 
 toggle.onclick = function (){
 	toggle.classList.toggle('active');

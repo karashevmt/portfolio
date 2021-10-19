895ef68073bd73f5b58260d7f1ffa9fedeaaeab2
karashevmt <karashevmarem@Gmail.com>
2021-10-19 15:27:51 +0300

    menu

--- js/main.js
+++ js/main.js
@@ -5,7 +5,12 @@ const main = document.querySelector('.main')
 const themeSwitch = document.querySelector('.themeSwitch')
 const mobileTheme = document.querySelector('.mobileTheme')
 const body = document.querySelector('body')
-
+const closeHome = document.querySelector('.close-home')
+const closeAbout = document.querySelector('.close-about')
+const closeServices = document.querySelector('.close-services')
+const closeProject = document.querySelector('.close-project')
+const closeTest = document.querySelector('.close-test')
+const closeContact = document.querySelector('.close-contact')
 
 
 themeSwitch.onclick = function () {
@@ -20,5 +25,45 @@ toggle.onclick = function (){
 	topbar.classList.toggle('active');
 	navigation.classList.toggle('active');
 	main.classList.toggle('active');
-
 }
+
+
+if (document.documentElement.clientWidth < 992) {
+	closeHome.onclick = function(){
+		toggle.classList.toggle('active');
+		topbar.classList.toggle('active');
+		navigation.classList.toggle('active');
+		main.classList.toggle('active');
+	}
+	closeTest.onclick = function(){
+		toggle.classList.toggle('active');
+		topbar.classList.toggle('active');
+		navigation.classList.toggle('active');
+		main.classList.toggle('active');
+	}
+	closeAbout.onclick = function(){
+		toggle.classList.toggle('active');
+		topbar.classList.toggle('active');
+		navigation.classList.toggle('active');
+		main.classList.toggle('active');
+	}
+	closeContact.onclick = function(){
+		toggle.classList.toggle('active');
+		topbar.classList.toggle('active');
+		navigation.classList.toggle('active');
+		main.classList.toggle('active');
+	}
+	closeProject.onclick = function(){
+		toggle.classList.toggle('active');
+		topbar.classList.toggle('active');
+		navigation.classList.toggle('active');
+		main.classList.toggle('active');
+	}
+	closeServices.onclick = function(){
+		toggle.classList.toggle('active');
+		topbar.classList.toggle('active');
+		navigation.classList.toggle('active');
+		main.classList.toggle('active');
+	}
+}
+
